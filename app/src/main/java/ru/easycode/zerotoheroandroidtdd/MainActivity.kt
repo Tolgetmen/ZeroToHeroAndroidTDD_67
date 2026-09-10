dc3eac0bbd96138de3a8f6a725b391c961dbe186
Tolgetmen <Tolgetmen@gmail.com>
2026-09-10 19:08:49 +0300

    Task 022part2

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -3,12 +3,12 @@ package ru.easycode.zerotoheroandroidtdd
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.core.view.children
 import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
-    private val textList = ArrayList<String>()
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,33 +17,32 @@ class MainActivity : AppCompatActivity() {
 
         binding.actionButton.setOnClickListener {
             val inputText = binding.inputEditText.text
-            textList.add(inputText.toString())
-            val textView = TextView(this).apply {
-                text = inputText
-            }
-            binding.contentLayout.addView(textView)
+            addTextView(inputText.toString())
             binding.inputEditText.text?.clear()
         }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putStringArrayList(KEY, textList)
+        val textList =
+            binding.contentLayout.children.map { (it as TextView).text.toString() }.toList()
+        outState.putStringArrayList(KEY, ArrayList(textList))
 
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
         val list = savedInstanceState.getStringArrayList(KEY) ?: ArrayList()
-        textList.addAll(list)
-        textList.forEach { inputText ->
-            val textView = TextView(this).apply {
-                text = inputText
-            }
-            binding.contentLayout.addView(textView)
+        list.forEach { inputText ->
+            addTextView(inputText)
         }
     }
-
+    fun addTextView(inputText: CharSequence){
+        val textView = TextView(this).apply {
+            text = inputText
+        }
+        binding.contentLayout.addView(textView)
+    }
     companion object {
         private const val KEY = "list"
     }
