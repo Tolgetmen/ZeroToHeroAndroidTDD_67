2eaa88974a755b87947882de9a69a0ca78e34a08
Tolgetmen <Tolgetmen@gmail.com>
2026-09-14 20:07:58 +0300

    Task 024

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -7,10 +7,36 @@ import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        viewModel = (application as App).viewModel
+
+        val adapter = Adapter()
+        binding.recyclerView.adapter = adapter
+        binding.actionButton.setOnClickListener {
+            val text = binding.inputEditText.text.toString()
+            viewModel.add(text)
+            binding.inputEditText.setText("")
+        }
+
+        viewModel.livedata().observe(this) {
+            adapter.update(it)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        viewModel.save(BundleWrapper.Base(outState))
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        viewModel.restore(BundleWrapper.Base(savedInstanceState))
     }
 }
+
