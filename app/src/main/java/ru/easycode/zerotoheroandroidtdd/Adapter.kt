63090774f7a73d6349cf24eb1ebf7f15a688ce4b
Tolgetmen <Tolgetmen@gmail.com>
2026-09-14 20:46:19 +0300

    Task 024part2

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/Adapter.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/Adapter.kt
@@ -2,6 +2,7 @@ package ru.easycode.zerotoheroandroidtdd
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import ru.easycode.zerotoheroandroidtdd.databinding.ItemLayoutBinding
@@ -21,9 +22,11 @@ class Adapter : RecyclerView.Adapter<ItemViewHolder>() {
     }
 
     fun update(newList: List<CharSequence>) {
+        val diffUtil = DiffutilCallback(list, newList)
+        val diff = DiffUtil.calculateDiff(diffUtil)
         list.clear()
         list.addAll(newList)
-        notifyDataSetChanged()
+        diff.dispatchUpdatesTo(this)
     }
 }
 
@@ -32,4 +35,27 @@ class ItemViewHolder(private val binding: ItemLayoutBinding) : ViewHolder(bindin
         binding.elementTextView.text = source
     }
 
+}
+
+private class DiffutilCallback(
+    private val old: List<CharSequence>,
+    private val new: List<CharSequence>
+) : DiffUtil.Callback() {
+    override fun getOldListSize() = old.size
+
+    override fun getNewListSize() = new.size
+
+    override fun areItemsTheSame(
+        oldItemPosition: Int,
+        newItemPosition: Int
+    ): Boolean {
+        return old[oldItemPosition] == new[newItemPosition]
+    }
+
+    override fun areContentsTheSame(
+        oldItemPosition: Int,
+        newItemPosition: Int
+    ): Boolean {
+        return old[oldItemPosition] == new[newItemPosition]
+    }
 }
